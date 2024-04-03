a05e300cf039476898e5ff61c93ee800680cf21f
kurahashi-kyohei <kurakyo18@gmail.com>
2024-04-03 15:38:20 +0900

    fix

--- src/app/components/menu/merchandisesContainer/index.tsx
+++ src/app/components/menu/merchandisesContainer/index.tsx
@@ -1,17 +1,16 @@
 import Style from './index.module.scss';
 import Image from "next/image";
 import Link from "next/link";
-import merchandises from "@/app/data/merchandises";
+import { merchandises } from '@/app/data/merchandises';
 
 const MerchandisesContainer: React.FC<{ path: string }> = ({ path }) => {
   return (
     <div className={Style.container}>
       {merchandises.map((merchandise, index) => {
         return(
-          (merchandise.href === path)
+          (merchandise.title === path)
           ?
           <div className={Style.merchandise}>
-            <Image className={Style.image} src={merchandise.image} alt={merchandise.title} width={350} height={300} />
             <div className={Style.text}>
               <h3>{merchandise.title}</h3>
               <h4>{merchandise.price}円 (税込)</h4>
