dc495dcac5744134ac6d7d7d89ee631218108df6
kurahashi-kyohei <kurakyo18@gmail.com>
2024-04-09 20:57:09 +0900

    fix

--- src/app/components/top/mv/index.tsx
+++ src/app/components/top/mv/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Style from './index.module.scss';
+import { mincho } from '@/app/styles/font';
 
 const Mv: React.FC = () => {
   return (
     <div id="mv" className={Style.mv}>
       <div className={Style.img_box}>
-        <h2>創業から半世紀以上<br />変わらぬ愛される味を<br />これからも守り続けて< br className={Style.sp_only}/>いきます。</h2>
         <div></div>
         <div></div>
       </div>
