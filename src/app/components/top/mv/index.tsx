608518234ef5294a2ed430615359602882f68594
kurahashi-kyohei <152674613+kurahashi-kyohei@users.noreply.github.com>
2024-04-11 21:42:44 +0900

    Revert "fix: トップページのMv修正"

--- src/app/components/top/mv/index.tsx
+++ src/app/components/top/mv/index.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import Style from './index.module.scss';
-import Image from 'next/image';
+import { mincho } from '@/app/styles/font';
 
 const Mv: React.FC = () => {
   return (
     <div id="mv" className={Style.mv}>
       <div className={Style.img_box}>
-        <Image src='/top/mv/bg_1.webp' alt='メインビジュアル' layout='fill' objectFit='cover'/>
-        <Image src='/top/mv/bg_2.webp' alt='メインビジュアル' layout='fill' objectFit='cover'/>
+        <div></div>
+        <div></div>
       </div>
-      
     </div>
   )
 }
