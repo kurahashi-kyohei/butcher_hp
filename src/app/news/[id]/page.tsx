f536912785f3a0b6e99fcf887f8ac599549fb724
kurahashi-kyohei <kurakyo18@gmail.com>
2024-04-26 23:05:48 +0900

    add: news_No3

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -2,15 +2,8 @@ import { NextPage } from 'next';
 import { usePathname } from 'next/navigation'; 
 import Mv from '@/app/components/news/mv';
 import NewsDetailContainer from '@/app/components/news/main/newsDetailContainer';
-import news from '@/app/data/news';
 
-export async function generateStaticParams(): Promise<any[]> {
-  return news.map((news) => ({
-    id: news.id.toString(),
-  }))
-}
-
-const NewsDetail = () => {
+const NewsDetail :NextPage = () => {
   const path = usePathname();
 
   return (
