6066a77f220baeeae679423e320274917e01b7cb
kurahashi-kyohei <kurakyo18@gmail.com>
2024-05-03 18:06:31 +0900

    fix

--- src/app/components/news/main/newsDetailContainer/index.tsx
+++ src/app/components/news/main/newsDetailContainer/index.tsx
@@ -1,37 +1,35 @@
-// 'use client'
+'use client'
 
-// import { News, getNews } from '../../../../../../lib/client';
-// import { useEffect, useState } from 'react';
-// import parse from 'html-react-parser';import Style from './index.module.scss';
+import { News, getNews } from '../../../../../../lib/client';
+import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';import Style from './index.module.scss';
 
+interface PathProps {
+  path: {
+    id: string;
+  };
+}
 
-// export default function NewsDetailContainer({ params }: { params: { slug: string } }) {
-//   // お知らせ詳細の読み込み
-//   const [news, setNews] = useState<News[]>();
-//   useEffect(() => {
-//     getNews(params.slug ?? '').then((news: News[]) => {
-//       setNews(news)
-//     })
-//   }, [params.slug]);
+export default function NewsDetailContainer({ path }: PathProps) {
+  // お知らせ詳細の読み込み
+  const [news, setNews] = useState<News>();
+  useEffect(() => {
+    getNews(path.id ?? '').then((news: News) => {
+      setNews(news)
+    })
+  }, [path.id]);
 
-//   return (
-//     <div className={Style.news_detail}>
-//       {news?.map((news, index) => {
-//         return(
-//           <div className={Style.item} key={news.id}>
-//             <div className={Style.title}>
-//               <h2>{news.title}</h2>
-//               <h3>{news.date}</h3>
-//             </div>
-//             <div className={Style.text}>
-//               {/* <div className={Style.desc} dangerouslySetInnerHTML={{ __html: news.desc }}></div> */}
-//               {parse(news?.content ?? '')}
-//             </div>
-            
-//           </div>
-//         )
-//       })}
-      
-//     </div>
-//   )
-// }
+  return (
+    <div className={Style.news_detail}>
+      <div className={Style.item} key={news?.id}>
+        <div className={Style.title}>
+          <h2>{news?.title}</h2>
+          <h3>{news?.date}</h3>
+        </div>
+        <p className={Style.desc}>
+          {parse(news?.content ?? '')}
+        </p>
+      </div>
+    </div>
+  )
+}
