75e58865c5226fc6e1798a84ac60ec6e63f719d2
kurahashi-kyohei <kurakyo18@gmail.com>
2024-06-04 16:47:46 +0900

    fix: calendar

--- src/app/components/parts/calendar/nextMonth/index.tsx
+++ src/app/components/parts/calendar/nextMonth/index.tsx
@@ -32,7 +32,7 @@ const NextMonth = () => {
     const date = new Date(year, month, i);
     const isSunday = date.getDay() === 0;
     //定休日を追加
-    const closeDay = [0];
+    const closeDay = [15];
     const isCloseDay = closeDay.includes(i);
 
     dates.push(
