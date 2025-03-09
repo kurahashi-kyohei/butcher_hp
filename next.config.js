d9eba9973320e3b3ec8eb2ba5dfc5698561ad43d
kurahashi-kyohei <kurakyo18@gmail.com>
2025-03-09 20:57:55 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  // Netlify用に設定
   images: {
-    unoptimized: true,  // Netlify用に画像最適化を無効化
+    unoptimized: true,
   },
 }
 
