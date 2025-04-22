c492251b56e23ef0490910b30307fcb665cfa85f
kurahashi-kyohei <kurakyo18@gmail.com>
2025-04-22 12:05:48 +0900

    fix schedule image

--- src/app/components/top/image/index.tsx
+++ src/app/components/top/image/index.tsx
@@ -5,7 +5,7 @@ import NextImage from 'next/image';
 const Image = () => {
   return (
     <div className={Style.img_wrapper}>
-      <NextImage src='/top/schedule.jpg' alt='年末年始の営業日' className={Style.img} layout='fill' objectFit='contain'/>
+      <NextImage src='/top/schedule/2025-GW.webp' alt='GWの営業日' className={Style.img} layout='fill' objectFit='contain'/>
     </div>
   )
 }
