d32ac7d501cad7076033fd15d038b5b554bc7d91
kurahashi-kyohei <kurakyo18@gmail.com>
2025-04-22 15:31:35 +0900

    fix

--- src/app/components/top/image/index.tsx
+++ src/app/components/top/image/index.tsx
@@ -6,6 +6,7 @@ const Image = () => {
   return (
     <div className={Style.img_wrapper}>
       <NextImage src='/top/schedule/2025-GW.webp' alt='GWの営業日' className={Style.img} layout='fill' objectFit='contain'/>
+      <NextImage src='/top/schedule/2025-GW-sp.webp' alt='GWの営業日' className={Style.img_sp} layout='fill' objectFit='contain'/>
     </div>
   )
 }
